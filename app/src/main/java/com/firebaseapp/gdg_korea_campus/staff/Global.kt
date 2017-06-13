f975f62ebc516a4a47f1488ae98b82238bec99f1
kyeongwan <lk@lemonlab.co.kr>
2017-06-14 01:12:54 +0900

    이벤트 리스트 DB 추가

--- app/src/main/java/com/firebaseapp/gdg_korea_campus/staff/Global.kt
+++ app/src/main/java/com/firebaseapp/gdg_korea_campus/staff/Global.kt
@@ -6,7 +6,7 @@ package com.firebaseapp.gdg_korea_campus.staff
 
 object Global{
 
-    val EVENTLIST_DB_URL = ""
+    val EVENTLIST_DB_URL = "https://script.google.com/macros/s/AKfycbx8o7-ELRZKY21ON_ELYNSffQDcHuBLvtRTqo0GpORwmemsiKQ/exec"
 
 
 }
