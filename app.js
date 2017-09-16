d455049117124025e00c9b3797f87ad6a425e874
강경완 <kw.kang@woowahan.com>
2017-09-16 13:02:44 +0900

    임시

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var https = require('https');
 var http = require('http');
 var fs = require('fs');
 var path = require('path');
+var request222 = require('request');
+var querystring = require('querystring');
 var port = process.env.PORT || 3000; //*
 
 http.createServer(function (request, response) {
@@ -11,6 +13,9 @@ http.createServer(function (request, response) {
     var filePath = '.' + request.url;
     if (filePath == './')
         filePath = './qr_check.html';
+
+    if(filePath == './admin')
+        filePath = './test.html';
 
     if (filePath.indexOf("./OAuth/") == 0){
         console.log(filePath);
@@ -46,10 +51,110 @@ http.createServer(function (request, response) {
         });
         
         request.end();
-    }
-    else {
+    }else if(filePath.indexOf("./api") == 0) {
+
+        request222.get('http://api.meetup.com/GDG-Seoul/events/242054608/rsvps?key=79a3f666e207865552d12428726849&fields=answers,pay_status&response=yes', function (error, response2, body) {
+          console.log('error:', error); // Print the error if one occurred
+          console.log('statusCode:', response2 && response2.statusCode); // Print the response status code if a response was received
+          // console.log('body:', body); // Print the HTML for the Google homepage.
+
+          var result = new Array();
+          var obj = JSON.parse(body);
+          console.log(obj.length);
+          for (var i = 0; i < obj.length; i++) {
+            var object = new Object();
+            object.name = obj[i].member.name;
+            object.userID = obj[i].member.id;
+            object.rsvps = obj[i].response;
+            object.rsvpedOn = obj[i].created;
+            object.price = obj[i].pay_status;
+            try{
+                object.photo = obj[i].member.photo.thumb_link;   
+            }catch(err){
+                object.photo = "";
+            }
+
+            try {
+                object.price = obj[i].pay_status;
+            }catch(err) {
+                object.price = "";
+            }
+
+            try{
+                object.answer = obj[i].answers[0].answer;
+            }catch(err){
+                object.answer = "";
+            }
+            console.log(object);
+            result.push(object);
+          }
 
 
+                response.writeHead(200, {'Content-Type' : 'application/json; charset=utf-8'});
+                response.end(JSON.stringify(result), 'utf-8');
+          
+        });
+
+        // var http2 = require('http');
+        // var request = http2.request({
+        //     hostname: 'api.meetup.com',
+        //     path: '/GDG-Campus/events/242668148/rsvps?key=9227e7f83811132d1e5e6a42197510',
+        //     method : 'GET'
+        // }, function(response2) {
+        //     var body = "";
+        //     console.log("bbb");
+        //     response2.on('data', function(contents) {
+        //         body += contents;
+        //     });
+
+        //     console.log(body);
+
+        //     response2.on('end', function() {
+        //         console.log(response2.statusCode);
+        //         response.writeHead(200, {'Content-Type' : 'application/json'});
+        //         response.end(body, 'utf-8');
+        //     });
+        // });
+        //  request.on('error', function(e) {
+        //     console.log("Error!", e.message);
+        // });
+
+        // console.log("aaa");
+    }else if(filePath.indexOf("./rsvp") == 0){
+        var jsonData = "";
+        request.on('data', function (chunk) {
+            jsonData += chunk;
+        });
+        request.on('end', function () {
+            console.log(jsonData);
+            var reqObj = JSON.parse(jsonData);
+            console.log(reqObj);
+            reqObj = JSON.parse(reqObj);
+            var member = reqObj.member;
+            console.log(member);
+            var status = reqObj.status;
+            console.log(status);
+
+             
+            request222.post({ 
+                url: 'http://api.meetup.com/GDG-Seoul/events/242054608/attendance?key=79a3f666e207865552d12428726849',
+                form:    {member : reqObj.member, status: reqObj.status},
+                headers: {
+                    'Content-Type' : 'application/x-www-form-urlencoded' 
+                    
+                },
+                
+                json: true },
+                function (error333, response333, body333) {
+                    console.log(body333)
+                    response.writeHead(200);
+                    response.end(JSON.stringify(body333));
+                
+            })
+        });
+                
+
+    }else {
         var extname = path.extname(filePath);
         var contentType = 'text/html';
         switch (extname) {
